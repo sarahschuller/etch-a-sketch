669a59c8462013dcc899103b54b1284f4bf67985
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-20 19:54:13 -0400

    Initial commit

---
+++ etch-a-sketch.js
@@ -0,0 +1,23 @@
+// Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+
+// Setup the canvas for drawing
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // Start the drawing
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
+
+// Drawing function
+
+// Handler for the keys
+
+// Clear/Shake function
+
+// Listen for arrow keys
