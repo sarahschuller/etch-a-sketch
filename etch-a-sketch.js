e7be5544dbfd46b101ee118dc6a6a0dfea383d1b
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-20 20:46:05 -0400

    add shake button functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -74,7 +74,13 @@ function handleKey(e){
 // Clear/Shake function
 function clearCanvas() {
     canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function() {
+        canvas.classList.remove('shake');
+    }, { once: true }
+    );
 }
 
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas)
