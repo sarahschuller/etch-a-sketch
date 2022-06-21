6c9bda8366f61c5148b811220056634a26d36bd8
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-20 20:24:30 -0400

    add switch statement key value

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
 
 // Setup the canvas for drawing
 
@@ -11,13 +12,13 @@ const { width, height } = canvas; // destructured version of lines above
 
 // Create random x & y starting points on the canvas
 
-const x = Math.floor(Math.random() * width);
-const y = Math.floor(Math.random() * width);
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * width);
 
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); // Start the drawing
 ctx.moveTo(x, y);
@@ -26,8 +27,23 @@ ctx.stroke();
 
 // Drawing function
 
-function draw(options) {
+function draw({ key }) {
+    console.log(key);
 
+    // Start the path
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    // Move X & Y values depending on user input
+    switch (key) {
+        case 'Arrowup':
+            y -= MOVE_AMOUNT;
+            break;
+        default:
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
 }
 
 // Handler for the keys
@@ -37,8 +53,6 @@ function handleKey(e){
     if (e.key.includes('Arrow')) {
         e.preventDefault();
         draw({ key: e.key });
-        console.log(e.key);
-        console.log('Handling Keys');
     }
     
 }
